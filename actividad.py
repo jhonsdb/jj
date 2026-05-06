fc626351baf91f4bf84736b41057280904b3d0a3
unknown <jhonsdb>
2026-05-06 11:42:51 +0200

    informacion añadida de ejercicios

--- actividad.py
+++ actividad.py
@@ -3,4 +3,8 @@ print(len(sample))###cuenta  los caracteres dentro de un string
 print(sample.lower())#convierte texto en minuscula
 print(sample.upper())#convierte texto en mayusculas
 print(sample.split())#separa el string en divisoria 
-print(sample.split("S"))#usamos la letra 'S' como separador.
+splitted = sample.split()
+separador = " "
+print(separador.join(splitted))#une lo separadoe por espacios en este caso
+print(sample.find("ple"))
+print(sample.replace("ple" , "e"))

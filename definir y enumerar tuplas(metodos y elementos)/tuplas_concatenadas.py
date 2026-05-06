e13b09bb69fb37364767c92851c60525165d5490
jhonsdb <you@example.com>
2026-05-06 22:27:41 +0200

    empaca y desempaca de tupla

---
+++ definir y enumerar tuplas(metodos y elementos)/tuplas_concatenadas.py
@@ -0,0 +1,14 @@
+#podemos agregar tupla de la siguiente manera 
+mytuple = (13,21,34,94,10,65)
+anothertupla = (22,25)
+combinedTuple = mytuple + anothertupla
+print(combinedTuple)
+
+#EMPACA Y DESEMPACA DE TUPLA
+
+mitupla= (13,21)
+(var1,var2)= mitupla
+print(var1)
+print(var2)
+packtulpa = var1,var2
+print(packtulpa)

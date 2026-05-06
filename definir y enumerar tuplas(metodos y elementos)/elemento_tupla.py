76e3c6b57e98611f6bd94c141fd626265b75e48b
jhonsdb <you@example.com>
2026-05-06 20:18:59 +0200

    datos faltantes en tupla

--- definir y enumerar tuplas(metodos y elementos)/elemento_tupla.py
+++ definir y enumerar tuplas(metodos y elementos)/elemento_tupla.py
@@ -1,5 +1,7 @@
 myTuple = (13,21,34,94,10,65)
-
+print(myTuple[2])
+print(myTuple[1:4])
+print(myTuple[-2])
 
 #cambiar tuplas
 #Las tuplas son uno de los tipos de datos inmutables en Python, lo que significa que no 
@@ -7,9 +9,8 @@ myTuple = (13,21,34,94,10,65)
 # explicamos cómo cambiar los elementos tupla, utilizando los métodos list() y tuple().
 
 
-tempList = list(myTuple)#cambia la tupla en lissta y la almacena en la variable
-print(type(tempList))# muetra el tipo de datto que hay en la variable en estet caso la lista
-tempList[3] = 999#sustituye en la posicion 3 el dattoo por el 333
-tempList.append(777)# añade a la lista el 777 en su ultimo indixe
-myTuple = tuple(tempList)#convierte la listya en tupla y lo alamacena en la variable
-print(myTuple)#muestra la tupla su  valor
+tempList = list(myTuple)
+tempList[3] = 999
+tempList.append(777)
+myTuple = tuple(tempList)
+print(myTuple)

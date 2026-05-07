c7f2baf9a54752bfc92ae08e59f370c158b901ba
jhonsdb <you@example.com>
2026-05-07 14:01:20 +0200

    elementtos de conjunto

---
+++ sets/elementos_de_conjunto.py
@@ -0,0 +1,3 @@
+#A diferencia de las listas o las tuplas, no se puede utilizar un índice para acceder a un elemento de un conjunto, pero se puede:
+#Iterar a través de los elementos de conjunto
+#Realice una verificación de membresía usando la palabra clave en
